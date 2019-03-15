f603c838bd46d819eeaec7f1b7d6cce843fcc0e1
martinvicknair <32040730+martinvicknair@users.noreply.github.com>
2019-03-15 11:56:12 -0600

    copy build files to root

---
+++ precache-manifest.d06f4b6d9ace6e07c0720e4d3921fe4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd0a07f7146e9b359794",
+    "url": "/static/js/main.c2e48737.chunk.js"
+  },
+  {
+    "revision": "8a1a7af79893cf21c720",
+    "url": "/static/js/2.7fe96521.chunk.js"
+  },
+  {
+    "revision": "bd0a07f7146e9b359794",
+    "url": "/static/css/main.eb626de3.chunk.css"
+  },
+  {
+    "revision": "8a1a7af79893cf21c720",
+    "url": "/static/css/2.d415239d.chunk.css"
+  },
+  {
+    "revision": "8dd0754269f330d1418f6fc62807cf71",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  // Button,
+  Container,
+  // Row,
+  // Col,
+  // Navbar,
+  // NavbarBrand,
+} from 'reactstrap';
+import Header from './components/Header/Header';
+import SectionsWrapper from './components/SectionsWrapper/SectionsWrapper';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+          <Container fluid>
+        <Header />
+        <SectionsWrapper>
+
+
+        </SectionsWrapper>
+        </Container>
+      )
+    }
 }
 
 export default App;
