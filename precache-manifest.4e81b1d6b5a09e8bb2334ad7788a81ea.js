d049d4ae7d4b25b90342962a8eeddb740cdd4349
Martin Vicknair <martinvicknair@gmail.com>
2019-03-24 20:32:19 -0600

    Updates

---
+++ precache-manifest.4e81b1d6b5a09e8bb2334ad7788a81ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "780bcbc715c88212df47",
+    "url": "/portfolio-react/static/js/runtime~main.162ca98c.js"
+  },
+  {
+    "revision": "6a04c49579b961cee4bc",
+    "url": "/portfolio-react/static/js/main.d94da683.chunk.js"
+  },
+  {
+    "revision": "31f706361f59666e72d8",
+    "url": "/portfolio-react/static/js/2.ea9d6500.chunk.js"
+  },
+  {
+    "revision": "6a04c49579b961cee4bc",
+    "url": "/portfolio-react/static/css/main.41734dfc.chunk.css"
+  },
+  {
+    "revision": "31f706361f59666e72d8",
+    "url": "/portfolio-react/static/css/2.520460bb.chunk.css"
+  },
+  {
+    "revision": "00b1f82886a86c86faa5e6f656593887",
+    "url": "/portfolio-react/index.html"
+  }
+];
